cda015f527dab4e2c75ad5e35ed2bf0822cb8f2e
TuxieNight <phowardsmith@wisc.edu>
2025-10-25 12:48:18 -0500

    Added event page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { HashRouter, Route, Routes } from 'react-router'
 import './App.css'
 import Home from './components/Home'
 import AboutMe from './components/AboutMe'
+import UpcomingEvents from './components/UpcomingEvents'
 
 function App() {
   return <HashRouter>
     <Routes>
       <Route path="/" element={<Home/>}></Route>
       <Route path="/about" element={<AboutMe/>}></Route>
-      <Route path="/upcoming-events" element={<upcomingEvents/>}></Route>
+      <Route path="/upcoming-events" element={<UpcomingEvents/>}></Route>
     </Routes>
   </HashRouter>
 }
--- src/components/upcomingEvents.jsx
+++ src/components/upcomingEvents.jsx
@@ -1,10 +1,11 @@
 import events from "../assets/events.json"
 
-export default function upcomingEvents(props) {
+export default function UpcomingEvents(props) {
     // get upcoming events
     const eventList = events.events.filter((e) => e.upcoming );
 
     return <div>
+        <h1>Upcoming Events</h1>
         {
             eventList.map((e) => {
                 return <Event key={e.name} {...e}></Event>
