3443ea539ec7cb632f2ffc644958d3b6f111dd1a
Khiem Vu <kmvu2@wisc.edu>
2025-10-28 11:27:25 -0500

    Layout done, temp Tutorial page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import { HashRouter, Route, Routes } from 'react-router'
 import './App.css'
+import Layout from './components/Layout'
 import Home from './components/Home'
 import AboutMe from './components/AboutMe'
 import UpcomingEvents from './components/UpcomingEvents'
 import PastEvents from './components/PastEvents'
 import Quizzes from './components/Quizzes'
+import Tutorials from './components/Tutorials'
 
 function App() {
   return <HashRouter>
     <Routes>
-      <Route path="/" element={<Home/>}></Route>
-      <Route path="/about" element={<AboutMe/>}></Route>
-      <Route path="/upcoming-events" element={<UpcomingEvents/>}></Route>
-      <Route path="/past-events" element={<PastEvents/>}></Route>
-      <Route path="/quizzes" element={<Quizzes/>}></Route>
+      {/* <Route path="/" element={<Home/>}></Route> */}
+      <Route path="/" element={<Layout/>}>
+        <Route index element={<Home/>} />
+        <Route path="/about" element={<AboutMe/>}></Route>
+        <Route path="/upcoming-events" element={<UpcomingEvents/>}></Route>
+        <Route path="/past-events" element={<PastEvents/>}></Route>
+        <Route path="/quizzes" element={<Quizzes/>}></Route>
+        <Route path="/tutorials" element={<Tutorials/>}></Route>
+      </Route>
     </Routes>
   </HashRouter>
 }
---
+++ src/components/Tutorials.jsx
@@ -0,0 +1,6 @@
+export default function Tutorials () {
+    // TODO
+    return <div style={{backgroundColor: "white", padding: "1.5rem", borderRadius: "1rem"}}>
+      <text>TODO</text>
+      </div>
+}
