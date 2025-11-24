736e0fa6680c2cf6319c0af526bf01409d27d8ad
WolfCipher <rhowardsmith@wisc.edu>
2025-11-24 16:18:27 -0600

    Hamburger icon added

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,19 +3,22 @@ import { Link, Outlet } from "react-router";
 
 export default function Layout(props) {
   return <div>
-    <Navbar className="mx-auto" bg="light" variant="light" style={{backgroundColor:"white", maxWidth: "fit-content", borderRadius: "1rem"}}>
+    <Navbar expand="md" className="mx-auto" bg="light" variant="light" style={{backgroundColor:"white", maxWidth: "fit-content", borderRadius: "1rem"}}>
       <Container>
         {/* <Navbar.Brand as={Link} to="/">
           <img/> TODO: create home image
         </Navbar.Brand> */}
-        <Nav className="mx-auto">
-          <Nav.Link as={Link} to="/">Home</Nav.Link>
-          <Nav.Link as={Link} to="/upcoming-events">Upcoming Events</Nav.Link>
-          <Nav.Link as={Link} to="/past-events">Past Events</Nav.Link>
-          <Nav.Link as={Link} to="/quizzes">Quizzes</Nav.Link>
-          <Nav.Link as={Link} to="/tutorials">Tutorials</Nav.Link>
-          <Nav.Link as={Link} to="/contact">Contact Us</Nav.Link>
-        </Nav>
+        <Navbar.Toggle aria-controls="main-nav" className="navbar-toggler-center" style={{color: "purple"}}/>
+        <Navbar.Collapse id="main-nav">
+          <Nav className="mx-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/upcoming-events">Upcoming Events</Nav.Link>
+            <Nav.Link as={Link} to="/past-events">Past Events</Nav.Link>
+            <Nav.Link as={Link} to="/quizzes">Quizzes</Nav.Link>
+            <Nav.Link as={Link} to="/tutorials">Tutorials</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contact Us</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
     <div style ={{ margin: "1rem", borderRadius: "1rem" }}>
