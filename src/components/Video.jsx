493fc0ad0e23499183b33a08b990b8b75ed4adc5
WolfCipher <rhowardsmith@wisc.edu>
2025-11-24 18:26:54 -0600

    Tutorial Video now turns into link if the device is small

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,13 +1,22 @@
 export default function Video({ src }) {
     return (
-        <iframe
-            src={src}
-            title="YouTube video player"
-            style={{ width: '100%', aspectRatio: '16/9', border: 0 }}
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerPolicy="strict-origin-when-cross-origin"
-            allowFullScreen
-        />
+        <>
+            {/* iframe visible on md+ */}
+            <div className="d-none d-md-block">
+                <iframe
+                    src={src}
+                    title="YouTube video player"
+                    style={{ width: '75%', aspectRatio: '16/9', border: 0 }}
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerPolicy="strict-origin-when-cross-origin"
+                    allowFullScreen
+                />
+            </div>
+            {/* link visible on sm */}
+            <div className="d-block d-md-none">
+                <a href={src}>{src}</a>
+            </div>
+        </>
     )
 }
