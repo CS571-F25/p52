5b61c5d7616a4a0a2a3e004c3fa85a388056f611
WolfCipher <rhowardsmith@wisc.edu>
2025-11-24 18:14:44 -0600

    edited width of video

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -3,7 +3,7 @@ export default function Video({ src }) {
         <iframe
             src={src}
             title="YouTube video player"
-            style={{ width: '75%', aspectRatio: '16/9', border: 0 }}
+            style={{ width: '100%', aspectRatio: '16/9', border: 0 }}
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerPolicy="strict-origin-when-cross-origin"
